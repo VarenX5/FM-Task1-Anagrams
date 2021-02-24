5ecb8282e9f7d2d163d23deb31de9140ae1bcb2e
Alexandr Daleckiy <varens05@gmail.com>
2021-02-24 19:14:52 +0800

    Added onTextChange for filter and got rid of redundant check.

--- app/src/main/java/com/foxminded/android/task1anagrams/MainActivity.java
+++ app/src/main/java/com/foxminded/android/task1anagrams/MainActivity.java
@@ -21,7 +21,22 @@ public class MainActivity extends AppCompatActivity {
         TextView wordsOutput = findViewById(R.id.textViewOutput);
         EditText filter = findViewById(R.id.editTextFilter);
         EditText userInput = findViewById(R.id.editTextUserInput);
+        filter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                wordsOutput.setText(Reversing.reverseWords(userInput.getText().toString(), filter.getText().toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         userInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -30,11 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(!(s.length()==0)){
-                    wordsOutput.setText(Reversing.reverseWords(s.toString(), filter.getText().toString()));
-                } else{
-                    wordsOutput.setText("");
-                }
+                wordsOutput.setText(Reversing.reverseWords(s.toString(), filter.getText().toString()));
             }
 
             @Override
