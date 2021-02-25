b099b29afe3fcf32608cce5bcdfb9183e49ac5f8
Alexandr Daleckiy <varens05@gmail.com>
2021-02-25 20:11:36 +0800

    Now algorithm checks from both sides

--- app/src/main/java/com/foxminded/android/task1anagrams/Reversing.java
+++ app/src/main/java/com/foxminded/android/task1anagrams/Reversing.java
@@ -17,22 +17,38 @@ public class Reversing {
 
     private static String reverseWord(String word, String filter){
         StringBuilder reversedWordBuilder = new StringBuilder(word);
-        int currentIndex = word.length()-1;
-        for (int y=0; y<word.length();y++){
-            if(!filter.contains(Character.toString(word.charAt(y)))){
-                if(filter.contains(Character.toString(reversedWordBuilder.charAt(currentIndex)))){
-                    while(filter.contains(Character.toString(reversedWordBuilder.charAt(currentIndex)))){
-                        currentIndex--;
+        int currentBackIndex = word.length()-1;
+        int currentFrontIndex = 0;
+        int wordBackIndex = word.length()-1;
+        for (int y=0; y<(word.length()/2);y++){
+            //checking from left to right
+            if(filter.indexOf(word.charAt(y))==-1){ // if character in original word is NOT in the filter
+                if(filter.indexOf(reversedWordBuilder.charAt(currentBackIndex))!=-1){ // if character in reversed word is in filter
+                    while(filter.indexOf(reversedWordBuilder.charAt(currentBackIndex))!=-1){ //skip if character in reversed word is in filter
+                        currentBackIndex--;
                     }
-                    reversedWordBuilder.setCharAt(currentIndex,word.charAt(y));
-                    currentIndex--;
+                    reversedWordBuilder.setCharAt(currentBackIndex,word.charAt(y)); //and after skipping we finally add character
+                    currentBackIndex--;
 
-                } else {
-                    reversedWordBuilder.setCharAt(currentIndex,word.charAt(y));
-                    currentIndex--;
-
+                } else { //else just add the character
+                    reversedWordBuilder.setCharAt(currentBackIndex,word.charAt(y));
+                    currentBackIndex--;
                 }
             }
+            //checking from right to left
+            if(filter.indexOf(word.charAt(wordBackIndex))==-1){
+                if(filter.indexOf(reversedWordBuilder.charAt(currentFrontIndex))!=-1){
+                    while(filter.indexOf(reversedWordBuilder.charAt(currentFrontIndex))!=-1){ //skip if character in reversed word is in filter
+                        currentFrontIndex++;
+                    }
+                    reversedWordBuilder.setCharAt(currentFrontIndex,word.charAt(wordBackIndex));
+                    currentFrontIndex++;
+                } else {
+                    reversedWordBuilder.setCharAt(currentFrontIndex,word.charAt(wordBackIndex));
+                    currentFrontIndex++;
+                }
+            }
+            wordBackIndex--;
         }
 
         return(reversedWordBuilder.toString());
