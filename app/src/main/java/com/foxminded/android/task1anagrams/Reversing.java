23d0e20609ea2b2da98c9874d4d3775df03d7e36
max <dangladste@gmail.com>
2021-02-25 14:45:58 +0200

    # hint

--- app/src/main/java/com/foxminded/android/task1anagrams/Reversing.java
+++ app/src/main/java/com/foxminded/android/task1anagrams/Reversing.java
@@ -5,50 +5,66 @@ public class Reversing {
 
     static String reverseWords(String userInput, String filter) {
         String[] inputWords = userInput.split("\\s");
-        String stringOutput = "";
+        StringBuilder stringOutput = new StringBuilder();
         for (String word : inputWords) {
-            stringOutput += " " + reverseWord(word, filter);
+            stringOutput.append(" ").append(reverseWord(word, filter));
         }
-        return (stringOutput.trim());
+        return stringOutput.toString().trim();
     }
 
     private static String reverseWord(String word, String filter) {
         StringBuilder reversedWordBuilder = new StringBuilder(word);
-        int currentBackIndex = word.length() - 1;
-        int currentFrontIndex = 0;
+        int rightIdx = word.length() - 1;
+        int leftIdx = 0;
+
+        while (rightIdx < leftIdx) {
+            if (isInFilter(filter, word.charAt(leftIdx))) {
+                // TODO: fill
+            } else if (isInFilter(filter, word.charAt(rightIdx))) {
+                // TODO: fill
+            } else {
+                // TODO: fill
+            }
+        }
+
+
         int wordBackIndex = word.length() - 1;
         for (int y = 0; y < (word.length() / 2); y++) {
             //checking from left to right
             if (filter.indexOf(word.charAt(y)) == -1) {
-                if (filter.indexOf(reversedWordBuilder.charAt(currentBackIndex)) != -1) {
-                    while (filter.indexOf(reversedWordBuilder.charAt(currentBackIndex)) != -1) {
-                        currentBackIndex--;
+                if (filter.indexOf(reversedWordBuilder.charAt(rightIdx)) != -1) {
+                    while (filter.indexOf(reversedWordBuilder.charAt(rightIdx)) != -1) {
+                        rightIdx--;
                     }
-                    reversedWordBuilder.setCharAt(currentBackIndex, word.charAt(y));
-                    currentBackIndex--;
+                    reversedWordBuilder.setCharAt(rightIdx, word.charAt(y));
+                    rightIdx--;
 
                 } else {
-                    reversedWordBuilder.setCharAt(currentBackIndex, word.charAt(y));
-                    currentBackIndex--;
+                    reversedWordBuilder.setCharAt(rightIdx, word.charAt(y));
+                    rightIdx--;
                 }
             }
             //checking from right to left
             if (filter.indexOf(word.charAt(wordBackIndex)) == -1) {
-                if (filter.indexOf(reversedWordBuilder.charAt(currentFrontIndex)) != -1) {
-                    while (filter.indexOf(reversedWordBuilder.charAt(currentFrontIndex)) != -1) {
-                        currentFrontIndex++;
+                if (filter.indexOf(reversedWordBuilder.charAt(leftIdx)) != -1) {
+                    while (filter.indexOf(reversedWordBuilder.charAt(leftIdx)) != -1) {
+                        leftIdx++;
                     }
-                    reversedWordBuilder.setCharAt(currentFrontIndex, word.charAt(wordBackIndex));
-                    currentFrontIndex++;
+                    reversedWordBuilder.setCharAt(leftIdx, word.charAt(wordBackIndex));
+                    leftIdx++;
                 } else {
-                    reversedWordBuilder.setCharAt(currentFrontIndex, word.charAt(wordBackIndex));
-                    currentFrontIndex++;
+                    reversedWordBuilder.setCharAt(leftIdx, word.charAt(wordBackIndex));
+                    leftIdx++;
                 }
             }
             wordBackIndex--;
         }
 
         return reversedWordBuilder.toString();
+    }
+
+    private static boolean isInFilter(String filter, char sign) {
+        return filter.indexOf(sign) != -1;
     }
 }
 
