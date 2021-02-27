8bfdc2c07aaec7e8fa96a6092797eab1c9cba0ef
Alexandr Daleckiy <varens05@gmail.com>
2021-02-27 21:16:44 +0800

    Requests fixed

--- app/src/androidTest/java/com/foxminded/android/task1anagrams/EspressoUITest.java
+++ app/src/androidTest/java/com/foxminded/android/task1anagrams/EspressoUITest.java
@@ -1,35 +1,23 @@
 package com.foxminded.android.task1anagrams;
 
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import com.squareup.spoon.Spoon;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.junit.Assert.assertEquals;
 
 @LargeTest
 public class EspressoUITest {
@@ -43,22 +31,19 @@ public class EspressoUITest {
         ViewInteraction appCompatEditTextInput = onView(
                 allOf(withId(R.id.editTextUserInput),
                         isDisplayed()));
-        appCompatEditTextInput.perform(replaceText("adhd5f"));
 
         ViewInteraction appCompatEditTextFilter = onView(
                 allOf(withId(R.id.editTextFilter),
                         isDisplayed()));
-        appCompatEditTextFilter.perform(replaceText("5"));
 
         ViewInteraction textViewOutput = onView(
                 allOf(withId(R.id.textViewOutput),
                         isDisplayed()));
-        textViewOutput.check(matches(withText("fdhd5a")));
 
-        String[] filterArray = {"", "abrc", "1234567890", "ab"};
-        String[] inputArray = {"abcdefg", "abrac", "afht5g", "abracadabra"};
-        String[] expectedArray = {"gfedcba", "abrac", "gthf5a", "abradacabra"};
-        for (int y = 0; y < 4; y++) {
+        String[] filterArray = {"", "abrc", "1234567890", "ab", "985sg"};
+        String[] inputArray = {"abcdefg", "abrac", "afht5g", "abracadabra","d98mrg s8he's sg5fsh"};
+        String[] expectedArray = {"gfedcba", "abrac", "gthf5a", "abradacabra","r98mdg s8'ehs sg5hsf"};
+        for (int y = 0; y < filterArray.length; y++) {
             appCompatEditTextInput.perform(replaceText(inputArray[y]));
             appCompatEditTextFilter.perform(replaceText(filterArray[y]));
             textViewOutput.check(matches(withText(expectedArray[y])));
@@ -66,13 +51,5 @@ public class EspressoUITest {
         }
     }
 
-    @Test(expected = NullPointerException.class)
-    public void espressoUINillExceptionTest() {
-        ViewInteraction appCompatEditTextInput = onView(
-                allOf(withId(R.id.editTextUserInput),
-                        isDisplayed()));
-        appCompatEditTextInput.perform(replaceText(null));
-
-    }
 
 }
--- app/src/test/java/com/foxminded/android/task1anagrams/ReversingLocalTest.java
+++ app/src/test/java/com/foxminded/android/task1anagrams/ReversingLocalTest.java
@@ -8,19 +8,26 @@ public class ReversingLocalTest {
 
     @Test
     public void reverseWords() {
-        String[] filterArray = {"", "abrc", "1234567890", "ab"};
-        String[] inputArray = {"abcdefg", "abrac", "afht5g", "abracadabra"};
-        String[] expectedArray = {"gfedcba", "abrac", "gthf5a", "abradacabra"};
-        for (int y = 0; y < 4; y++) {
+        String[] filterArray = {"", "abrc", "1234567890", "ab", "985sg"};
+        String[] inputArray = {"abcdefg", "abrac", "afht5g", "abracadabra","d98mrg s8he's sg5fsh"};
+        String[] expectedArray = {"gfedcba", "abrac", "gthf5a", "abradacabra","r98mdg s8'ehs sg5hsf"};
+        for (int y = 0; y < filterArray.length; y++) {
             String actual = Reversing.reverseWords(inputArray[y], filterArray[y]);
             assertEquals(actual, expectedArray[y]);
         }
     }
 
     @Test(expected = NullPointerException.class)
-    public void reverseWordsTestNullInput() {
+    public void reverseWordsTestNullFilter() {
         String filter = null;
         String input = "Rocks";
+        Reversing.reverseWords(input, filter);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void reverseWordsTestNullInput() {
+        String filter = "123";
+        String input = null;
         Reversing.reverseWords(input, filter);
     }
 
